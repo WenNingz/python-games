e24084a35b38bb8e5bb5a2e024f62688a0c99eec
Ning <wenning.me@gmail.com>
2019-02-14 21:57:06 +0900

    Add game source code

---
+++ jokes.py
@@ -0,0 +1,18 @@
+# Short jokes
+
+print('What do you get wehn you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+print('What do dentists call a astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
